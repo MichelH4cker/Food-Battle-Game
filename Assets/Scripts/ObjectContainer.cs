c26aa1a8b20bb1440e7edd9d1148b7bfe307660b
MichelH4cker <michel.hecker@usp.br>
2022-02-28 11:39:47 -0300

    card-beaviour: connect objectContainer with objectDrag using GameManager

---
+++ Assets/Scripts/ObjectContainer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectContainer : MonoBehaviour {
+    
+    public bool isFull;
+    public GameManager gameManager;
+
+    private void Start() {
+        gameManager = GameManager.instance;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        
+        if ((gameManager.draggingObject != null) && (isFull == false)) {
+            gameManager.currentContainer = this.gameObject;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision) {
+        gameManager.currentContainer = null;
+    }
+}
