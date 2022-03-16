7b6df7843c928f9f465d617af8667d9b26174175
MichelH4cker <michel.hecker@usp.br>
2022-03-16 09:32:33 -0300

    enemies-behavior: create basic enemies movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,8 +7,9 @@ public class EnemyController : MonoBehaviour {
     public Vector3 FinalDestination;
     public int Health;
     public int Damage;
+    public float movementSpeed;
 
     void Update() {
-        
+         transform.Translate(new Vector3(movementSpeed * -1, 0, 0));
     }
 }
