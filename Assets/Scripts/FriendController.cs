00bb6b8769a8791f9fd4e8e44740a9604dc6bb48
MichelH4cker <michel.hecker@usp.br>
2022-03-16 19:10:03 -0300

    enemies-behavior: create bullet Prefab

--- Assets/Scripts/FriendController.cs
+++ Assets/Scripts/FriendController.cs
@@ -3,5 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FriendController : MonoBehaviour {
-   public List<GameObject> enemies;
+    public GameObject bullet;
+    public List<GameObject> enemies;
+    public bool isAttacking;
+    public float attackCooldown;
+    private float attackTime;
+
+    private void Update() {
+        if (enemies.Count > 0 && isAttacking == false){  
+            isAttacking = true;
+        } else if (enemies.Count  == 0 && isAttacking == true) {
+            isAttacking = false;
+        }
+    }
 }
