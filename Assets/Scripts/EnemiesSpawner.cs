d0dde8316f4e4613cf89430b720fb72659e18178
MichelH4cker <michel.hecker@usp.br>
2022-03-19 11:36:34 -0300

    card-behavior: add enemies random spawn

--- Assets/Scripts/EnemiesSpawner.cs
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -8,7 +8,11 @@ public class EnemiesSpawner : MonoBehaviour  {
 
     private void Update(){
         foreach(Enemy enemy in enemies) {
-            if(enemy.isSpawned == false && enemy.spawnTime <= Time.time){
+            if(enemy.isSpawned == false && enemy.spawnTime <= Time.time) {
+                if(enemy.randomSpawn) {
+                    enemy.spawner = Random.Range(0, transform.childCount);
+                }
+
                 GameObject enemyInstance = Instantiate(enemiesPrefabs[(int)enemy.enemyType], transform.GetChild(enemy.spawner).transform);
 
                 transform.GetChild(enemy.spawner).GetComponent<SpawnPoint>().enemies.Add(enemyInstance);
