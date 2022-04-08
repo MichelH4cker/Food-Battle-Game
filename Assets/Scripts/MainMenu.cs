53ce7d8bfbd4ce4d2d38f1f37c21195495079a5b
MichelH4cker <michel.hecker@usp.br>
2022-04-07 21:14:58 -0300

    scenes: create Loading and switch Scenes functions

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
     public void PlayGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneLoader.Load(SceneLoader.Scene.GameScene);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame() {
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader {
+    
+    public enum Scene {
+        MenuScene,
+        LoadingScene,
+        GameScene,
+        EndScene,
+    }
+
+    private static Scene targetScene;
+
+    public static void Load(Scene scene) {
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+       
+        targetScene = scene;
+    }
+
+    public static void LoadTargetScene() {
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+
+}
